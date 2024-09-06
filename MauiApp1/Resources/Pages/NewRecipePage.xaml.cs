da22491cf26cf5ea877d92de98ddddd33b68c011
haeun-yang <c3413958@uon.edu.au>
2024-09-06 23:10:12 +1000

    Add camera feature (pickphoto)

--- MauiApp1/Resources/Pages/NewRecipePage.xaml.cs
+++ MauiApp1/Resources/Pages/NewRecipePage.xaml.cs
@@ -6,6 +6,31 @@ public partial class NewRecipePage : ContentPage
 	{
 		InitializeComponent();
 	}
+    public async Task<PermissionStatus> GetCameraPermission()
+    {
+        PermissionStatus status = await Permissions.RequestAsync<Permissions.Camera>();
+
+        if (status == PermissionStatus.Granted)
+            return status;
+
+        if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            // Prompt the user to turn on in settings
+            // On iOS once a permission has been denied it may not be requested again from the application
+            await DisplayAlert("Warning", "You need to manually enable camera access for this app in settings.", "OK");
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.Camera>())
+        {
+            // Prompt the user with additional information as to why the permission is needed
+            await DisplayAlert("Warning", "This app requires camera access to add photos for your character", "OK");
+        }
+
+        status = await Permissions.RequestAsync<Permissions.Camera>();
+
+        return status;
+    }
     private async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var recipeName = RecipeNameEntry.Text;
@@ -29,18 +54,17 @@ public partial class NewRecipePage : ContentPage
 
     private async void OnAddPhotoButtonClicked(object sender, EventArgs e)
     {
-        // 사진 파일 선택
-        var result = await FilePicker.Default.PickAsync(PickOptions.Images);
-
+        FileResult result = null;
+        // 사진 선택 로직 (예: MediaPicker를 사용할 수 있음)
+        PermissionStatus status = await GetCameraPermission();
+        if (status == PermissionStatus.Granted)
+        {
+            result = await MediaPicker.PickPhotoAsync();
+        }
         if (result != null)
         {
-            // 선택한 파일의 스트림을 가져오기
-            using (var stream = await result.OpenReadAsync())
-            {
-                // 이미지를 Image 컨트롤에 표시
-                RecipeImage.Source = ImageSource.FromStream(() => stream);
-                RecipeImage.IsVisible = true; // 이미지를 표시
-            }
+            var stream = await result.OpenReadAsync();
+            RecipeImage.Source = ImageSource.FromStream(() => stream);
         }
     }
 }
--- MauiApp1/Resources/Pages/NewRestaurantPage.xaml.cs
+++ MauiApp1/Resources/Pages/NewRestaurantPage.xaml.cs
@@ -6,11 +6,41 @@ public partial class NewRestaurantPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    public async Task<PermissionStatus> GetCameraPermission()
+    {
+        PermissionStatus status = await Permissions.RequestAsync<Permissions.Camera>();
+
+        if (status == PermissionStatus.Granted)
+            return status;
+
+        if (status == PermissionStatus.Denied && DeviceInfo.Platform == DevicePlatform.iOS)
+        {
+            // Prompt the user to turn on in settings
+            // On iOS once a permission has been denied it may not be requested again from the application
+            await DisplayAlert("Warning", "You need to manually enable camera access for this app in settings.", "OK");
+            return status;
+        }
+
+        if (Permissions.ShouldShowRationale<Permissions.Camera>())
+        {
+            // Prompt the user with additional information as to why the permission is needed
+            await DisplayAlert("Warning", "This app requires camera access to add photos for your character", "OK");
+        }
+
+        status = await Permissions.RequestAsync<Permissions.Camera>();
+
+        return status;
+    }
     // 사진 추가 버튼 클릭 이벤트 처리
     private async void OnAddPhotoButtonClicked(object sender, EventArgs e)
     {
+        FileResult result = null;
         // 사진 선택 로직 (예: MediaPicker를 사용할 수 있음)
-        var result = await MediaPicker.PickPhotoAsync();
+        PermissionStatus status = await GetCameraPermission();
+           if (status == PermissionStatus.Granted)
+           {
+               result = await MediaPicker.PickPhotoAsync();
+           }
         if (result != null)
         {
             var stream = await result.OpenReadAsync();
